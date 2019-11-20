d8c5469c0d0f155b9f6759ec0b07e52a8853fad4
Prima Aulia Gusta <primaulia@gmail.com>
2019-11-20 14:45:21 +0800

    ytd livecode

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,17 +1,21 @@
 class PetsController < ApplicationController
   def index
+    @pets = Pet.all
   end
 
   def create
   end
 
   def new
+    @new_pet = Pet.new #blank instance
   end
 
   def edit
+    @edited_pet = Pet.find(params[:id])
   end
 
   def show
+    @pet = Pet.find(params[:id])
   end
 
   def update
